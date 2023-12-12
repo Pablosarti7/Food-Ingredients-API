3b15ccdafcb5393cfbb067817d10bc86a35b20d4
Pablosarti7 <paulsarti7@gmail.com>
2023-12-12 12:33:00 -0500

    Created new table

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("POSTGRES_DB_URI")
 db = SQLAlchemy(app)
 
 
-class Ingredient(db.Model):
+class Ingredients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
-
-    def __repr__(self):
-        return f"{self.name}"
-
+    rating = db.Column(db.String, nullable=False)
+    
 
 @app.route("/")
 def home():
@@ -29,28 +27,30 @@ def home():
 ## HTTP GET - Read Record
 @app.route("/all")
 def get_all_cafes():
-    all_ingredients = db.session.query(Ingredient).all()
+    all_ingredients = db.session.query(Ingredients).all()
     all_data = []
     for ingredient in all_ingredients:
         ingredients = dict(name=ingredient.name,
-                     description=ingredient.description
+                     description=ingredient.description,
+                     rating=ingredient.description,
                     )
         all_data.append(ingredients)
 
     return jsonify(ingredients=all_data)
 
 
-## HTTP GET - Read Record
+# ## HTTP GET - Read Record
 @app.route("/ingredients", methods=["GET"])
 def search_ingredients():
     loc_data = request.args.get('name')
-    ingredients = Ingredient.query.filter_by(name=loc_data.title()).all()
+    ingredients = Ingredients.query.filter_by(name=loc_data.title()).all()
     if ingredients: # if the data requested exist
         all_data = ingredients
         data_list = []
         for data in all_data:
             data_dict = dict(name=data.name,
-                       description=data.description
+                       description=data.description,
+                       rating=data.rating,
                         )
             data_list.append(data_dict)
 
@@ -75,11 +75,11 @@ def post_ingredient():
     if request.method == "POST":
         data = request.json
 
-        # Check if data is a list of items
-        if isinstance(data, list):
+        
+        if isinstance(data, list): # Check if data is a list of items
             for item in data:
                 process_ingredient(item)
-        elif isinstance(data, dict):  # It's a single item
+        elif isinstance(data, dict): # It's a single item
             process_ingredient(data)
         else:
             abort(400, description="Bad Request: Invalid data format.")
@@ -92,15 +92,16 @@ def post_ingredient():
 def process_ingredient(item):
     name = item.get('name')
     description = item.get('description')
+    rating = item.get('rating')
 
-    if not name or not description:
-        abort(400, description="Bad Request: 'name' and 'description' fields are required.")
+    if not name or not description or not rating:
+        abort(400, description="Bad Request: 'name', 'description', and 'rating' fields are required.")
 
-    existing_ingredient = Ingredient.query.filter_by(name=name).first()
+    existing_ingredient = Ingredients.query.filter_by(name=name).first()
     if existing_ingredient:
         abort(400, description="An ingredient with this name already exists.")
 
-    new_ingredient = Ingredient(name=name, description=description)
+    new_ingredient = Ingredients(name=name, description=description, rating=rating)
     db.session.add(new_ingredient)
     db.session.commit()
 
@@ -115,7 +116,7 @@ def delete_cafe(id):
     
     if request.method == "DELETE":
             try:
-                ingredient_to_delete = Ingredient.query.get(id)
+                ingredient_to_delete = Ingredients.query.get(id)
                 db.session.delete(ingredient_to_delete)
                 db.session.commit()
                 return jsonify(response={"Success": "Successfully deleted the cafe from the database."})
