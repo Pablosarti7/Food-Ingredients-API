c6216cf19543ef6ba568803570f53090751b546b
Pablosarti7 <paulsarti7@gmail.com>
2024-12-13 11:44:15 -0500

    Changes the if main script

--- main.py
+++ main.py
@@ -125,3 +125,10 @@ def delete_cafe(id):
 
 if __name__ == '__main__':
     app.run(debug=False)
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 5000)
+    host = "0.0.0.0"
+    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
+    app.run(host=host, port=port, debug=debug_mode)
