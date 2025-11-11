c20501048102bdcc7bc3444916a1e0575dc6852b
Pablo Sarti <paulsarti7@gmail.com>
2025-11-11 18:16:37 -0500

    Changes name for db url

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("POSTGRES_DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
 
 
